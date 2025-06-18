6ba85fa9c621d915bd45116058ce73eefc3c086a
Modou Lamin Manneh <mlangsco@gmail.com>
2025-06-18 19:39:24 +0200

    Update uppg2.js

--- uppg2.js
+++ uppg2.js
@@ -1,9 +1,8 @@
 
 function uppg2() {
-   // Skriv din kod här
    // deklarera en variable med ditt namn och skriv ut det i konsolen
-
-
+   const namn = "Modou";
+   console.log(namn);
 }
 
 module.exports = { uppg2 };
