de401ef78ac744af27b5197bf3494b0a2150691f
Modou Lamin Manneh <mlangsco@gmail.com>
2025-06-20 17:10:41 +0200

    uppg1.js

--- uppg1.js
+++ uppg1.js
@@ -1,6 +1,5 @@
 function uppg1() {
-  console.log('Hello World!');
+  console.log("Hello World!");
 }
-uppg1();
-// This function prints "Hello World!" to the console.
+
 module.exports = { uppg1 };
